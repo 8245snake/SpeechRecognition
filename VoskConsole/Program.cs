c429815275fd9ecfefb147e1d5df16e794799716
Hanyu <8245snake@gmail.com>
2022-05-18 23:46:58 +0900

    プロジェクト ファイルを追加します。

---
+++ VoskConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using Vosk;
+
+Model model = new Model(@"path\to\model");
+
+using (var waveIn = new WaveInEvent())
+{
+    waveIn.WaveFormat = new WaveFormat(44100, 1);
+
+    var rec = new VoskRecognizer(model, waveIn.WaveFormat.SampleRate);
+    rec.SetMaxAlternatives(0);
+    rec.SetWords(true);
+
+    waveIn.DataAvailable += (_, e) =>
+    {
+        
+        if (rec.AcceptWaveform(e.Buffer, e.BytesRecorded))
+        {
+            Console.WriteLine(rec.Result());
+        }
+        else
+        {
+            Console.WriteLine(rec.PartialResult());
+        }
+
+    };
+    waveIn.StartRecording();
+    Console.WriteLine("Press ENTER to quit...");
+    Console.ReadLine();
+    waveIn.StopRecording();
+}
+
+Console.WriteLine("Program ended successfully.");
